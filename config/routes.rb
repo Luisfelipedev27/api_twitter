ffbaf42534d3c7518bd0db1b589ffef5a56bbe4f
Luis Felipe <luisfelipe1582@hotmail.com>
2024-05-04 16:58:59 -0300

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: [:show, :create] do
+        post 'create_post', on: :member
+      end
+
+      resources :posts, only: [:index, :create] do
+        member do
+          post :repost
+        end
+      end
+    end
+  end
 end
